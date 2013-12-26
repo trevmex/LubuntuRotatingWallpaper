287975ba2d9d1490f174a590d6a2611d84ad7d16
Trevor Lalish-Menagh <trev@trevreport.org>
2013-12-26 13:40:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+= Rotating Wallpaper for Lubuntu
+
+This is the same script as described at http://www.reddit.com/r/wallpapers/comments/1tqe9k/update_new_version_of_the_8bit_day_wallpaper_set/ but tweaked for Lubuntu and Bash.
+
+For original install notes, go to https://www.dropbox.com/s/swz6k954o4y5osh/Install-and-shizzle.txt
+
+Mush love to BloodyMarvellous and OhMrBigshot for compiling this together.
---
+++ update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+files=(
+    01-Morning.png
+    02-Late-Morning.png
+    03-Afternoon.png
+    04-Late-Afternoon.png
+    05-Evening.png
+    06-Late-Evening.png
+    07-Night.png
+    08-Late-Night.png
+)
+
+#Timings for the backgrounds in order. Your life may vary.
+timing=(
+    7
+    10
+    12
+    17
+    18
+    19
+    21
+    23
+)
+
+hour=`date +%H`
+
+#setcmd="gsettings set org.gnome.desktop.background picture-uri"
+setcmd="pcmanfm -w"
+
+for i in {7..0..-1}
+do
+    if [[ $hour -ge ${timing[i]} ]]; then
+        $setcmd $DIR/${files[i]}
+        echo "Wallpaper set to $DIR/${files[i]}"
+        exit
+    fi
+done
+
+$setcmd $DIR/${file[8]}
+echo "Wallpaper set to $DIR/${file[8]}"
